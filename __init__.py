2fc9946a7850900a5cae575d167d525f39b37f67
Abhishek Singh <abhishek.ec@global.org.in>
2026-07-10 14:29:18 +0530

    Update __init__.py with version and author info
    Added metadata and backward compatibility aliases for GBRF.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
-from .gbrf import GBRF
+"""
+GBRF - Gradient Boosted Random Forest
+A hybrid machine learning algorithm combining Random Forest and Gradient Boosting.
+"""
+
+__version__ = "2.0.0"
+__author__ = "Abhishek Singh"
+__all__ = ["GBRF", "GBRFClassifier", "GBRFRegressor"]
+
+from .gbrf import GBRF, GRFRegressor, GRFClassifier
+
+# Backward compatibility alias
+GBRFRegressor = GRFRegressor
+GBRFClassifier = GRFClassifier
