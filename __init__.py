5f2dce5b4784f71d4b05cca514bfcd52f7f43d5a
Abhishek Singh <abhishek.ec@global.org.in>
2026-07-10 15:39:27 +0530

    Update module name and version in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 """
-GBRF - Gradient Boosted Random Forest
+hybridgbrf - Gradient Boosted Random Forest
 A hybrid machine learning algorithm combining Random Forest and Gradient Boosting.
 """
 
-__version__ = "2.0.0"
+__version__ = "1.0.0"
 __author__ = "Abhishek Singh"
 __all__ = ["GBRF", "GBRFClassifier", "GBRFRegressor"]
 
 from .gbrf import GBRF, GRFRegressor, GRFClassifier
 
 # Backward compatibility alias
-GBRFRegressor = GRFRegressor
-GBRFClassifier = GRFClassifier
+GBRF = GRFRegressor
